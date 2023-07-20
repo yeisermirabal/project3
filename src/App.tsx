3fa998c5eb9f76ae0086b05a5ae427d423d42cbd
yeisermirabal <yeisermirabal@gmail.com>
2023-07-20 17:55:59 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/**COmponents */
+import Header from './components/Layout/Header';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+/**Styles */
+import styles from './Header.module.css'
+
+const Header = () => {
+    return (<></>)
+}
+
+export default Header;
